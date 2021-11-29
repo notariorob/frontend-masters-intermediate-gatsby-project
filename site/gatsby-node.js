78ba9d00fd1285f4b0e665e04319211c80ef92e8
Roberto Notario <notariorob@gmail.com>
2021-11-29 00:10:52 +0000

    add remote file notes and optimized images

--- site/gatsby-node.js
+++ site/gatsby-node.js
@@ -1,3 +1,51 @@
+const fetch = require('node-fetch');
+const { createRemoteFileNode } = require('gatsby-source-filesystem');
+
+const authors = require('./src/data/authors.json');
+const books = require('./src/data/books.json');
+
+exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
+  const { createNode, createTypes } = actions;
+
+  createTypes(`
+    type Author implements Node {
+      books: [Book!]! @link(from: "slug" by: "author.slug")
+    }
+
+    type Book implements Node {
+      author: Author! @link(from: "author" by: "slug")
+    }
+  `);
+
+  authors.forEach((author) => {
+    createNode({
+      ...author,
+      id: createNodeId(`author-${author.slug}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: 'Author',
+        content: JSON.stringify(author),
+        contentDigest: createContentDigest(author),
+      },
+    });
+  });
+
+  books.forEach((book) => {
+    createNode({
+      ...book,
+      id: createNodeId(`book-${book.isbn}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: 'Book',
+        content: JSON.stringify(book),
+        contentDigest: createContentDigest(book),
+      },
+    });
+  });
+};
+
 exports.createPages = ({ actions }) => {
   const { createPage } = actions;
 
@@ -11,4 +59,55 @@ exports.createPages = ({ actions }) => {
       },
     },
   });
+};
+
+exports.createResolvers = ({
+  actions,
+  cache,
+  createNodeId,
+  createResolvers,
+  store,
+  reporter,
+}) => {
+  const { createNode } = actions;
+  const resolvers = {
+    Book: {
+      buyLink: {
+        type: 'String',
+        resolve: ({ isbn }) =>
+          `https://www.powells.com/searchresults?keyword=${isbn}`,
+      },
+      cover: {
+        type: 'File',
+        resolve: async ({ isbn, name }) => {
+          const response = await fetch(
+            `https://openlibrary.org/isbn/${isbn}.json`,
+          );
+
+          if (!response.ok) {
+            reporter.warn(
+              `Error loading details about ${name} - got ${response.status}: ${response.statusText}`,
+            );
+            return null;
+          }
+
+          const { covers } = await response.json();
+          if (covers.length) {
+            return createRemoteFileNode({
+              url: `https://covers.openlibrary.org/b/id/${covers[0]}-L.jpg`,
+              store,
+              cache,
+              createNode,
+              createNodeId,
+              reporter,
+            });
+          } else {
+            return null;
+          }
+        },
+      },
+    },
+  };
+
+  createResolvers(resolvers);
 };
